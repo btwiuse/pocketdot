9e0651250f38bd512cb1d175c8ade04273858591
navigaid <navigaid@gmail.com>
2023-12-17 16:47:40 +0800

    codegen: specify llvm path

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ apk:
 	flutter build apk --split-per-abi --target-platform android-arm64
 
 codegen:
-	flutter_rust_bridge_codegen -r smoldot-flutter/src/api.rs -d lib/bridge_generated.dart
+	flutter_rust_bridge_codegen -r smoldot-flutter/src/api.rs -d lib/bridge_generated.dart --llvm-path /usr/lib/llvm-14/lib/libclang.so.1
 
 init:
 	cargo install cargo-expand
--- lib/bridge_generated.dart
+++ lib/bridge_generated.dart
@@ -295,7 +295,7 @@ class SmoldotFlutterWire implements FlutterRustBridgeWireBase {
       : _lookup = lookup;
 
   void store_dart_post_cobject(
-    int ptr,
+    DartPostCObjectFnType ptr,
   ) {
     return _store_dart_post_cobject(
       ptr,
@@ -303,10 +303,10 @@ class SmoldotFlutterWire implements FlutterRustBridgeWireBase {
   }
 
   late final _store_dart_post_cobjectPtr =
-      _lookup<ffi.NativeFunction<ffi.Void Function(ffi.Int)>>(
+      _lookup<ffi.NativeFunction<ffi.Void Function(DartPostCObjectFnType)>>(
           'store_dart_post_cobject');
-  late final _store_dart_post_cobject =
-      _store_dart_post_cobjectPtr.asFunction<void Function(int)>();
+  late final _store_dart_post_cobject = _store_dart_post_cobjectPtr
+      .asFunction<void Function(DartPostCObjectFnType)>();
 
   Object get_dart_object(
     int ptr,
@@ -518,3 +518,8 @@ final class wire_uint_8_list extends ffi.Struct {
   @ffi.Int32()
   external int len;
 }
+
+typedef DartPostCObjectFnType = ffi.Pointer<
+    ffi.NativeFunction<
+        ffi.Bool Function(DartPort port_id, ffi.Pointer<ffi.Void> message)>>;
+typedef DartPort = ffi.Int64;
